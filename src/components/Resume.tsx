d029b4e84a2dac605981f11655560862f6c32545
Peter Zhang <34387360+Ultrasword@users.noreply.github.com>
2025-02-11 23:32:12 -0500

    vercel weird

---
+++ src/components/Resume.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+
+import styles from "../sections/Contact.module.css";
+
+const ResumeContainer = () => {
+  return (
+    <div className={styles["resume-container"]}>
+      <a
+        href="https://www.canva.com/design/DAGeiavI1Fo/XIM8XYruf72ekkVaK_VlDw/view?embed"
+        style={{ width: "100%" }}
+      >
+        <iframe
+          src="https://www.canva.com/design/DAGeiavI1Fo/XIM8XYruf72ekkVaK_VlDw/view?embed"
+          width="100%"
+          height="500px"
+          style={{ border: "none" }}
+          allowFullScreen={true}
+        ></iframe>
+      </a>
+    </div>
+  );
+};
+
+export default ResumeContainer;
