082408d857937994f02a2e30bf3331904e713d7c
Peter Zhang <34387360+Ultrasword@users.noreply.github.com>
2025-04-18 15:32:22 -0400

    changed resume

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -1,1 +1,3 @@
-export const RESUME_LINK: string = "Peter_Zhang_Resume_2025.pdf";
+export const RESUME_LINK: string =
+  // "Peter_Zhang_Resume_2025.pdf";
+  "https://drive.google.com/file/d/1YmvYcyr3rkkHRfhWreyGh0CmWpjqPSt4/view?usp=sharing";
